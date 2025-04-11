baea094ef9f409a80c17a8d631b05d460f8b5f9b
André Wendt <awendt@freeshell.org>
2025-04-11 14:17:45 +0200

    chore: remove dead code

--- test/reducer.js
+++ test/reducer.js
@@ -59,32 +59,6 @@ describe('Reducer', async () => {
     ]);
   });
 
-  it('returns closed PRs #notificationsForClosedPRs', async () => {
-    const reducer = new NotificationReducer({ notifications });
-    reducer.pullRequests = [ pr21, pr45 ];
-
-    assert.equal(reducer.notificationsForClosedPRs.length, 1);
-    assert.equal(reducer.notificationsForClosedPRs[0].subject.url, "https://api.github.com/repos/babbel/terraform-aws-acm/pulls/21");
-  });
-
-  it('returns closed PRs #notificationsForReassignedPRs', async () => {
-    const notifications = await github.notifications();
-    const reducer = new NotificationReducer({ notifications, me: 'awendt' });
-    reducer.pullRequests = [ pr21, pr45 ];
-
-    assert.equal(reducer.notificationsForReassignedPRs.length, 1);
-    assert.equal(reducer.notificationsForReassignedPRs[0].subject.url, "https://api.github.com/repos/babbel/terraform-aws-athena/pulls/45");
-  });
-
-  it('returns closed PRs #notificationsForReviewedPRs', async () => {
-    const notifications = await github.notifications();
-    const reducer = new NotificationReducer({ notifications });
-    reducer.pullRequests = [ pr21, pr45 ];
-
-    assert.equal(reducer.notificationsForReviewedPRs.length, 1);
-    assert.equal(reducer.notificationsForReviewedPRs[0].subject.url, "https://api.github.com/repos/babbel/terraform-aws-acm/pulls/21");
-  });
-
   describe('#applyRules', async () => {
     let reducer;
 
